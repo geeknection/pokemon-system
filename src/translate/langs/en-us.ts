87c83f8c50ee40f98325ce8d70c5f12710c0fca2
Bruno Nascimento <bruno.nascimento.ava@gmail.com>
2021-01-31 19:13:07 -0300

    Idioma inglês

---
+++ src/translate/langs/en-us.ts
@@ -0,0 +1,25 @@
+const en_US: any = {
+    welcome: 'Welcome',
+    lets_go: 'Lets go!',
+    loading: 'Loading',
+    systemTitle: 'Pokemon Test',
+    search: 'Search',
+    search_placeholder: 'Search for a Pokemon in the list below ...',
+    pokemons_list: 'List of Pokemon',
+    not_found: 'No Pokemon found',
+    see: 'Show more',
+    pokemonPage: 'Pokemon page',
+    goBack: 'Back',
+    details: 'Details',
+    height: 'Height',
+    weight: 'Weight',
+    stats: 'Base status',
+    abilities: 'Skills',
+    types: 'Types',
+    ability: 'Ability',
+    type: 'Type',
+    seeDetails: 'View details',
+    close: 'Close'
+};
+
+export default en_US;
